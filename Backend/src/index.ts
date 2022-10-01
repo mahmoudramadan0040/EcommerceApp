1c428b7239d6daa90766f6c970b5336fc93ba8eb
mahmoudramamdan0040 <mahmoudramamdan0040@gmail.com>
2022-10-01 22:08:20 +0200

    mongoose and Error handler added

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,18 +1,24 @@
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
-import * as dotenv from 'dotenv'
-dotenv.config()
-const PORT = process.env.PORT || 3000
+import dbConnection from '../Database/index';
+import config from "../config/config";
+import ErrMiddleware from "./middlewares/err";
+const PORT =config.port || 3000
 // create an instance server
 const app: Application = express()
+dbConnection();
 // HTTP request logger middleware
 app.use(morgan('dev'))
+app.use(express.json());
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World 🌍'
   })
 })
+// error handler middelware 
+const err=  new ErrMiddleware();
+app.use(err.error);
 // start express server
 app.listen(PORT, () => {
   console.log(`Server is starting at prot:${PORT}`)
